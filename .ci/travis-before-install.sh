0927eb4c7c3a0b10ecef59959f4898538b26070c
Harlan T Wood <code+harlan@harlantwood.net>
2018-04-03 11:23:04 -1000

    WIP

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -5,8 +5,6 @@ sudo apt-get update
 # sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 
 sudo rm -f `which docker-compose`
-sudo rm -f `which docker-compose`
-sudo rm -f `which docker-compose`
 echo $PATH
 curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
 chmod +x ~/docker-compose
